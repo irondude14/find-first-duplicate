9064f5069d822c7c1c6c282269e66a6a261f2aa4
Vladyslav <vkadyrov@protonmail.com>
2023-10-02 19:39:58 -0400

    Solved.

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,19 +1,30 @@
 function findFirstDuplicate(arr) {
-  // type your code here
+  const duplicate = new Set();
+
+  for (let i = 0; i < arr.length; i++) {
+    if (duplicate.has(arr[i])) {
+      return arr[i];
+    } else {
+      duplicate.add(arr[i]);
+    }
+  }
+
+  return -1;
 }
 
 if (require.main === module) {
   // add your own tests in here
-  console.log("Expecting: 3");
-  console.log("=>", findFirstDuplicate([2, 1, 3, 3, 2]));
+  console.log('Expecting: 3');
+  console.log('=>', findFirstDuplicate([2, 1, 3, 3, 2]));
 
-  console.log("");
+  console.log('');
 
-  console.log("Expecting: -1");
-  console.log("=>", findFirstDuplicate([1, 2, 3, 4]));
+  console.log('Expecting: -1');
+  console.log('=>', findFirstDuplicate([1, 2, 3, 4]));
 }
 
 module.exports = findFirstDuplicate;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+// Iterate through the array, compare each value to the next one, with the first match, return it. If there is no match, return -1
--- ruby/find_first_duplicate.rb
+++ ruby/find_first_duplicate.rb
@@ -1,18 +1,28 @@
+require 'set'
+
 def find_first_duplicate(arr)
-  # type your code in here
+  duplicate = Set.new
+
+  arr.each do |i|
+    return i if duplicate.include?(i)
+    duplicate.add(i)
+  end
+
+  return -1
 end
 
 if __FILE__ == $PROGRAM_NAME
-  puts "Expecting: 3"
-  puts "=>", find_first_duplicate([2, 1, 3, 3, 2])
+  puts 'Expecting: 3'
+  puts '=>', find_first_duplicate([2, 1, 3, 3, 2])
 
   puts
 
-  puts "Expecting: -1"
-  puts "=>", find_first_duplicate([1, 2, 3, 4])
+  puts 'Expecting: -1'
+  puts '=>', find_first_duplicate([1, 2, 3, 4])
 
   # Don't forget to add your own!
 end
 
 # Please add your pseudocode to this file
 # And a written explanation of your solution
+# Same thing, we can utilize Sets and a loop to find duplicates
